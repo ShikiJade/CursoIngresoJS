e61b76c5446424a9b1123ec3aec27b34b4faab08
ShikiJade <40763644+ShikiJade@users.noreply.github.com>
2018-07-02 12:06:14 -0300

    Ejercicios 1, 2 y 3.
    Primeros 3 ejercicios ya resueltos.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,18 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	/* desde w3school
+	var person = prompt("Please enter your name", "Harry Potter");
+	(Comentario en bloque)
+	*/
+//Comentario de linea
+var nombre;
+nombre=prompt("Inserte su nombre de usuario para la sesión", "NN" );
+alert (nombre);
+
+/* var define variables , las cuales quedan guardadas
+y pueden ser usadas cuando se necesiten con el nombre
+que se le puso.*/
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,19 @@
 /*Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	/*
+	alert("ejercicio 3");
+	console.log("Esto es un log");
+	*/
+
+//NO OLVIDAR PUNTO Y COMA
+var nombre;
+nombre=document.getElementById('elNombre').value;
+alert (nombre);
+
+
+//document. get (tab) Shift + I para mayor velocidad
 }
 
 
