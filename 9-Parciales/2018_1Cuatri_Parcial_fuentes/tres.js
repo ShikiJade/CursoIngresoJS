583327f4aa1759dc2d39b6d7296b18550c945073
ShikiJade <40763644+ShikiJade@users.noreply.github.com>
2018-07-03 08:03:36 -0300

    Ejercicios 1, 2 y 3
    Ejercicios 1 y 2 del modelo de parcial resueltos totalmente. Ejercicio 3 a mitad.

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,11 @@
 function mostrar()
 {
-
+	var precio;
+	precio=prompt("Escriba el precio del producto");
+	var descuento
+	descuento=prompt("Escriba el porcentaje de descuento");
+	var preciofinal;
+	preciofinal=precio*descuento%;
+	alert(preciofinal);
+//aprender como colocar los porcentajes.
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,13 @@
 
 function mostrar()
 {
+var ancho;
+ancho=prompt("Inserte el ancho del rectángulo.");
+var largo;
+largo=prompt("Inserte el largo del rectángulo.");
+var perimetro;
+perimetro=(2*largo)+(2*ancho);
+
+alert(perimetro);
 
 }
