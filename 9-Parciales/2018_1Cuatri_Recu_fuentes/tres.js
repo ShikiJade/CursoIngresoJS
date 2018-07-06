97973be7172e96a6b20fe6762afaf8ecc6547d40
ShikiJade <40763644+ShikiJade@users.noreply.github.com>
2018-07-06 12:04:57 -0300

    Resueltos en clase
    Ejercicios de practica de parcial resueltos en clase.

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,26 @@
 function mostrar()
 {
 
+	var precio;
+	var porcentajedescuento;
+	var dinerodescuento;
+	var preciodescuento;
+	var IVA;
+	var preciototal;
+
+	precio=prompt ("Inserte el precio");
+	porcentajedescuento=prompt ("Inserte el descuento");
+
+	dinerodescuento=precio*porcentajedescuento/100;
+	preciodescuento=precio-dinerodescuento;
+	IVA=preciodescuento*21/100;
+	preciototal=
+
+	alert ("La primer cuenta es "+dinerodescuento+"  la segunda da "+preciodescuento+"  la tercera da "+IVA);
+
+
+
+
+
+
 }
